373136d9782dc82e08ea43d1774d8e56c68da8a4
cc1533 <cc1533@msstate.edu>
2016-03-05 10:46:36 -0600

    Update server.java

--- server.java
+++ server.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.Formatter;
 
 public class server
-{
+{	// java server localhost 3000 3001 output.txt
 	public static void main(String args[]) throws IOException
 	{
 		InetAddress emulatorName = InetAddress.getByName(args[0]);
@@ -11,32 +11,91 @@ public class server
 		int sendToEmulator = Integer.parseInt(args[2]);
 		String fileName = args[3];
         	ServerSocket nSocket = new ServerSocket(receiveFromEmulator);
-		int packSize = 4;
+		//int packSize = 37;
 		boolean eot = false;
 		
-		// Creating UDP connection with Client.
-		DatagramSocket servSock = new DatagramSocket(receiveFromEmulator);
+		// Creating UDP connection with Emulator.
+		DatagramSocket fromClient = new DatagramSocket(receiveFromEmulator);
+		DatagramSocket toClient = new DatagramSocket(sendToEmulator);
 		// Creating "received.txt" file.
 		Formatter file = new Formatter(fileName);
-		
+		// using this byte array fixed the EOFException error I was getting.
+		byte[] incomingData = new byte[1024];
+		byte[] outgoingData = new byte[1024];
+		// expected sequence number of the first packet is always 0.
+		int exSeqNum = 0;
 		while(!eot)
 		{
 			// Receive packet from Client.
-			byte[] recData = new byte[packSize];
-			DatagramPacket recPack = new DatagramPacket(recData, recData.length);
-			servSock.receive(recPack);
+			DatagramPacket recPack = new DatagramPacket(incomingData, incomingData.length);
+			fromClient.receive(recPack);
 			// Deserialize packet
-			ByteArrayInputStream baStream = new ByteArrayInputStream(recData);
-			//System.out.println(baStream);
+			byte[] data = recPack.getData();
+			ByteArrayInputStream baStream = new ByteArrayInputStream(data);
 			ObjectInputStream oiStream = new ObjectInputStream(baStream);
 			// Initialize the string to jump over any errors.
 			String received = new String();
 			try
 			{
-				// I keep getting an "EOFException" here... idk what's going on.
 				packet packet = (packet) oiStream.readObject();
-				received = new String(packet.getData());
-				packet.printContents();
+				oiStream.close();
+				// if the packet is a data packet
+				if(packet.getType() == 1)
+				{
+					//System.out.println("Received Data Packet.");
+					// % 8 means the client doens't have to reset the seq #s every time.
+					if(exSeqNum != (packet.getSeqNum() % 8))
+					{
+						// drop the packet and send the previous seqNum as an ack
+						System.out.println("Packet did not contain the expected sequence #.");
+						packet nack = new packet(2,exSeqNum - 1, 0, null);
+						// copied from client.java
+						ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+						ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
+						ooStream.writeObject(nack);
+						ooStream.flush();
+						outgoingData = baoStream.toByteArray();
+						//System.out.println("Sending:  ");
+						//nack.printContents();
+						DatagramPacket sendNack = new DatagramPacket(outgoingData, outgoingData.length, emulatorName, sendToEmulator);
+						// as of right now, the client doesn't know what to do with any acks so that needs to be added
+						// along side the implementation of the timer.
+						toClient.send(sendNack);
+					}
+					// if it is the expected seq num, increment seqnum, send ack, add data to file
+					else
+					{
+						//System.out.println("Packet contained the expected sequence #.");
+						received = new String(packet.getData());
+						packet.printContents();
+						// send ack
+						packet ack = new packet(0,packet.getSeqNum(),0,null);
+						ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+						ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
+						ooStream.writeObject(ack);
+						ooStream.flush();
+						outgoingData = baoStream.toByteArray();
+						//System.out.println("Sending:  ");
+						//ack.printContents();
+						DatagramPacket sendAck = new DatagramPacket(outgoingData, outgoingData.length, emulatorName, sendToEmulator);
+						toClient.send(sendAck);
+						// Write data received to the file.
+						file.format("%s", received);
+						exSeqNum++;
+					}
+				}
+				// packet is an EOT from the client
+				else if(packet.getType() == 3)
+				{
+					eot = true;
+					file.close();		// Close output file.
+					fromClient.close();	// Close UDP socket.
+					toClient.close();
+				}
+				else
+				{
+					System.out.println("Error, unexpected packet type sent to server.");
+				}
 			} catch (ClassNotFoundException e)
 			{
 				e.printStackTrace();
@@ -44,19 +103,12 @@ public class server
 			
 
 			// Test for eof signal
-			if(received.contains("XEOF"))
+			/*if(received.contains("XEOF"))
                         {
 				file.close();
 				servSock.close();
 				System.exit(0);
-			}
-			// Write data received to the file.
-			file.format("%s", received);
-			// Send Ack to Client.
-			/*String ack = received.toUpperCase();
-			byte[] sendAck = ack.getBytes();
-			DatagramPacket sendPack = new DatagramPacket(sendAck, sendAck.length, recPack.getAddress(), recPack.getPort());
-			servSock.send(sendPack);*/
+			}*/
 		}
 	}//end main
 }//end server
