fd81a37a3922482ca241c58fc43983544ba64ad1
jh2866 <jh2866@msstate.edu>
2016-03-10 19:57:58 -0600

    Added files via upload

--- client.java
+++ client.java
@@ -1,184 +1,232 @@
 /***********************************************************************************************************************************************
-*	Created by Christopher Cole (cc1533) on 3/1/2016.
-*	CSE 4153, Programming Assignment 2, client.java
-*
-* TODO for whole project
-*	1) DONE -- Get basic UDP connection to server set up.
-*	2) DONE -- Get basic file transfer working.
-*	3) IN PROGRESS -- Figure out Go-Back-N protocol and get it working.
-*	4) DONE -- Get serialization and deserialization working.
-*	5) DONE -- Send file by packets class.
-*	6) DONE -- Get the window working.
-*	7) Set up timer for last packet with no ack.
-*	8) Set up seqNum logs and Ack logs for client.
-*	9) If there's time, clean everything up and try to use methods instead of having everything in main.
-*
-************************************************************************************************************************************************/
+ *	Created by Christopher Cole (cc1533) on 3/1/2016.
+ *	CSE 4153, Programming Assignment 2, client.java
+ *
+ * TODO for whole project
+ *	1) DONE -- Get basic UDP connection to server set up.
+ *	2) DONE -- Get basic file transfer working.
+ *	3) IN PROGRESS -- Figure out Go-Back-N protocol and get it working.
+ *	4) DONE -- Get serialization and deserialization working.
+ *	5) DONE -- Send file by packets class.
+ *	6) DONE -- Get the window working.
+ *	7) Set up timer for last packet with no ack.
+ *	8) Set up seqNum logs and Ack logs for client.
+ *	9) If there's time, clean everything up and try to use methods instead of having everything in main.
+ *
+ ************************************************************************************************************************************************/
 
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.io.Serializable.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class client
 {	// java client localhost 3000 3001 test.txt
-	public static void main(String args[]) throws IOException
-	{
-		InetAddress emulatorName = InetAddress.getByName(args[0]);
-		int sendToEmulator = Integer.parseInt(args[1]);
-		int receiveFromEmulator = Integer.parseInt(args[2]);
-		String fileName = args[3];
-
-		// Set up File stuff.
-		Scanner scanFile = new Scanner(new File(fileName));
-		String fileString = "";			
-		// Copy all the file's contents into a string, the string is what will be used and modified.
-		while(scanFile.hasNext())
-		{
-			fileString += scanFile.nextLine() + "\n";   // "\n" added because .nextLine() leaves it off by default.
-		}
-		scanFile.close();
-
-		// Create UDP connections between client and emulator, they say "toServer" and "fromServer" for simplicity.
-		DatagramSocket toServer = new DatagramSocket();
-		DatagramSocket fromServer = new DatagramSocket(receiveFromEmulator);
-
-
-
-		// Setting up variables to be used in the while loop.
-                String[] fileSubString = new String[8];
-		int payloadSize = 30;
-                int i = 0;	// current location of the window in relation to the file.
-                boolean endOfFile = false;
-		int exAckNum = 0;
-		
-		// Change this to work for final ack instead of endOfFile.
-		while(!endOfFile)
-		{
-			int[] eachPackLen = new int[8];
-			exAckNum = 0;	// reset exAckNum every new window.
-			//boolean ack = false;
-			System.out.println("Starting at byte:  " + i + " from the file.");
-			
-			// This for-loop splits the fileString into segments of the given payloadSize.  As long as i is less than the length of
-			// 	the fileString - the payloadSize, it is not the final packet of the transmission.
-			// Else, it is the final packet and sets the endOfFile variable accordingly.
-			int windowSize = 7;
-			for(int j = 0; j <= windowSize; j++)
-			{
-				// for all packets before the end of the file.
-				if(i < (fileString.length() - payloadSize))
-				{
-					fileSubString[j] = fileString.substring(i, i+payloadSize);
-					i += payloadSize;
-				}
-				// For the final packet to the end of the file.
-				else if(i >= (fileString.length() - payloadSize))
-				{
-					fileSubString[j] = fileString.substring(i, fileString.length());
-					endOfFile = true;
-				}
-				// if the current packet is the end of file packet, all packets after this one don't contain any information.
-				if(endOfFile)
-				{
-					System.out.println("End of File found.");
-					for(int k = j + 1; k <= windowSize; k++)
-					{
-						fileSubString[k] = "";
-						j++;
-					}
-				}
-			}
-
-			// Sends all the packets to the server one after another.
-			for(int j = 0; j <= windowSize; j++)
-			{
-				packet payLoad = new packet(1,j,fileSubString[j].length(),fileSubString[j]);
-				eachPackLen[j] = fileSubString[j].length();
-				// Serialize packet
-				ByteArrayOutputStream baStream = new ByteArrayOutputStream();
-				ObjectOutputStream ooStream = new ObjectOutputStream(baStream);
-				ooStream.writeObject(payLoad);
-				ooStream.flush();
-				byte[] sendData = baStream.toByteArray();
-				System.out.println("Sending:  ");
-				payLoad.printContents();
-				DatagramPacket packet = new DatagramPacket(sendData, sendData.length, emulatorName, sendToEmulator);
-				toServer.send(packet);
-				// global timer should start here with a loop looking for the ack after the for loop
-			}
-
-			// This is the GBN protocol *currently in progress*.
-			for(int j = 0; j <= windowSize; j++)
-			{
-				byte[] ackFromServer = new byte[1024];
-				DatagramPacket recAck = new DatagramPacket(ackFromServer, ackFromServer.length);
-				fromServer.receive(recAck);
-				byte[] ackD = recAck.getData();
-				ByteArrayInputStream bais = new ByteArrayInputStream(ackD);
-				ObjectInputStream ois = new ObjectInputStream(bais);
-				// obtained ack from server, test ack seqNums
-				try
-				{
-					packet ackP = (packet) ois.readObject();
-					ois.close();
-					System.out.println("Expected Sequence #:  " + exAckNum + " -- Received Sequence #:  " + ackP.getSeqNum());
-					if(exAckNum == ackP.getSeqNum())
-					{
-						//reset timer for next packet
-						exAckNum++;
-					}
-					// if the expected ack is less than ackP.getSeqNum() then the server got the exAckNum packet but the
-					// ack from the server was dropped, there's no need to resend data in this case.
-					else if(exAckNum < ackP.getSeqNum())
-					{
-						System.out.println("Accepting Ack from server as cumulative.");
-						exAckNum = ackP.getSeqNum() + 1;
-					}
-					// if the exAckNum is greater than ackP.getSeqNum(), the exAckNum packet was dropped on the way to the
-					// server, resend all data starting with exAckNum.
-					else if(exAckNum > ackP.getSeqNum())
-					{
-						System.out.println("Packet was dropped going to server, resending data.");
-						for(int k = exAckNum; k <= windowSize; k++)
-						{
-							System.out.println("Subtracting: " + eachPackLen[k] + " From:  " + i);
-							i = i - eachPackLen[k];
-							System.out.println("New i:  " + i);
-						}
-						if(endOfFile)
-						{
-							// if a packet drop occurred, the data needs to be resent.
-							endOfFile = false;
-						}		
-						// breaks out of the for-loop, there's no need to check further for more drops.
-						j = windowSize + 1;
-					}
-				} catch(ClassNotFoundException e)
-				{
-					e.printStackTrace();
-				}
-			}
-
-			
-			// If the endOfFile packet is sent, create and send the EOT packet
-			if(endOfFile)
-			{
-				// This will basically be its own mini sender and receiver that uses a while-loop to wait for ack.
-				//String endOfFileS = "XEOF";
-				packet eofpayLoad = new packet(3,0,0,null);
-				ByteArrayOutputStream eofStream = new ByteArrayOutputStream();
-				ObjectOutputStream ooeofStream = new ObjectOutputStream(eofStream);
-				ooeofStream.writeObject(eofpayLoad);
-				ooeofStream.flush();
-				byte[] eofSig = eofStream.toByteArray();
-				DatagramPacket sendEOF = new DatagramPacket(eofSig, eofSig.length, emulatorName, sendToEmulator);
-				toServer.send(sendEOF);
-				// Start timer and look for ack of EOT packet from server
-
-			}
-		}//end while
-		toServer.close();	// Connections terminated.
-		fromServer.close();
-	}//end main
+    public static void main(String args[]) throws IOException
+    {
+        InetAddress emulatorName = InetAddress.getByName(args[0]);
+        int sendToEmulator = Integer.parseInt(args[1]);
+        int receiveFromEmulator = Integer.parseInt(args[2]);
+        String fileName = args[3];
+        
+        // Set up File stuff.
+        Scanner scanFile = new Scanner(new File(fileName));
+        String fileString = "";
+        // Copy all the file's contents into a string, the string is what will be used and modified.
+        while(scanFile.hasNext())
+        {
+            fileString += scanFile.nextLine() + "\n";   // "\n" added because .nextLine() leaves it off by default.
+        }
+        scanFile.close();
+        
+        // Create UDP connections between client and emulator, they say "toServer" and "fromServer" for simplicity.
+        DatagramSocket toServer = new DatagramSocket();
+        DatagramSocket fromServer = new DatagramSocket(receiveFromEmulator);
+        
+        /* * * * * Log Files * * * * */
+        // Sequence number log file
+        Logger seqNumLog = Logger.getLogger("seqnum");
+        FileHandler fhSeq;
+        
+        try {
+            
+            // This block configure the logger with handler and formatter
+            fhSeq = new FileHandler("%h/Downloads/seqnum.log"); // save the log file
+            seqNumLog.addHandler(fhSeq);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fhSeq.setFormatter(formatter);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // Ack log file
+        Logger ackLog = Logger.getLogger("ack");
+        FileHandler fhAck;
+        
+        try {
+            
+            // This block configure the logger with handler and formatter
+            fhAck = new FileHandler("%h/Downloads/ack.log"); // save the log file
+            ackLog.addHandler(fhAck);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fhAck.setFormatter(formatter);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        /* * * * * End of Log Files * * * * */
+        
+        // Setting up variables to be used in the while loop.
+        String[] fileSubString = new String[8];
+        int payloadSize = 30;
+        int i = 0;	// current location of the window in relation to the file.
+        boolean endOfFile = false;
+        int exAckNum = 0;
+        
+        // Change this to work for final ack instead of endOfFile.
+        while(!endOfFile)
+        {
+            int[] eachPackLen = new int[8];
+            exAckNum = 0;	// reset exAckNum every new window.
+            //boolean ack = false;
+            System.out.println("Starting at byte:  " + i + " from the file.");
+            
+            // This for-loop splits the fileString into segments of the given payloadSize.  As long as i is less than the length of
+            // 	the fileString - the payloadSize, it is not the final packet of the transmission.
+            // Else, it is the final packet and sets the endOfFile variable accordingly.
+            int windowSize = 7;
+            for(int j = 0; j <= windowSize; j++)
+            {
+                // for all packets before the end of the file.
+                if(i < (fileString.length() - payloadSize))
+                {
+                    fileSubString[j] = fileString.substring(i, i+payloadSize);
+                    i += payloadSize;
+                }
+                // For the final packet to the end of the file.
+                else if(i >= (fileString.length() - payloadSize))
+                {
+                    fileSubString[j] = fileString.substring(i, fileString.length());
+                    endOfFile = true;
+                }
+                // if the current packet is the end of file packet, all packets after this one don't contain any information.
+                if(endOfFile)
+                {
+                    System.out.println("End of File found.");
+                    for(int k = j + 1; k <= windowSize; k++)
+                    {
+                        fileSubString[k] = "";
+                        j++;
+                    }
+                }
+            }
+            
+            // Sends all the packets to the server one after another.
+            for(int j = 0; j <= windowSize; j++)
+            {
+                packet payLoad = new packet(1,j,fileSubString[j].length(),fileSubString[j]);
+                eachPackLen[j] = fileSubString[j].length();
+                // Serialize packet
+                ByteArrayOutputStream baStream = new ByteArrayOutputStream();
+                ObjectOutputStream ooStream = new ObjectOutputStream(baStream);
+                ooStream.writeObject(payLoad);
+                ooStream.flush();
+                byte[] sendData = baStream.toByteArray();
+                System.out.println("Sending:  ");
+                payLoad.printContents();
+                DatagramPacket packet = new DatagramPacket(sendData, sendData.length, emulatorName, sendToEmulator);
+                toServer.send(packet);
+                
+                // global timer should start here with a loop looking for the ack after the for loop
+            }
+            
+            // This is the GBN protocol *currently in progress*.
+            for(int j = 0; j <= windowSize; j++)
+            {
+                byte[] ackFromServer = new byte[1024];
+                DatagramPacket recAck = new DatagramPacket(ackFromServer, ackFromServer.length);
+                fromServer.receive(recAck);
+                byte[] ackD = recAck.getData();
+                ByteArrayInputStream bais = new ByteArrayInputStream(ackD);
+                ObjectInputStream ois = new ObjectInputStream(bais);
+                // obtained ack from server, test ack seqNums
+                try
+                {
+                    packet ackP = (packet) ois.readObject();
+                    ois.close();
+                    //if (ackP.getLength() != 0)
+                    //{
+                        // Update SeqNum log file
+                        seqNumLog.info(Integer.toString(ackP.getSeqNum()%8));
+                        // Update Ack log file
+                        ackLog.info(Integer.toString(ackP.getSeqNum()%8));
+                    //}
+                    System.out.println("Expected Sequence #:  " + exAckNum + " -- Received Sequence #:  " + ackP.getSeqNum());
+                    if(exAckNum == ackP.getSeqNum())
+                    {
+                        //reset timer for next packet
+                        exAckNum++;
+                    }
+                    // if the expected ack is less than ackP.getSeqNum() then the server got the exAckNum packet but the
+                    // ack from the server was dropped, there's no need to resend data in this case.
+                    else if(exAckNum < ackP.getSeqNum())
+                    {
+                        System.out.println("Accepting Ack from server as cumulative.");
+                        exAckNum = ackP.getSeqNum() + 1;
+                    }
+                    // if the exAckNum is greater than ackP.getSeqNum(), the exAckNum packet was dropped on the way to the
+                    // server, resend all data starting with exAckNum.
+                    else if(exAckNum > ackP.getSeqNum())
+                    {
+                        System.out.println("Packet was dropped going to server, resending data.");
+                        for(int k = exAckNum; k <= windowSize; k++)
+                        {
+                            System.out.println("Subtracting: " + eachPackLen[k] + " From:  " + i);
+                            i = i - eachPackLen[k];
+                            System.out.println("New i:  " + i);
+                        }
+                        if(endOfFile)
+                        {
+                            // if a packet drop occurred, the data needs to be resent.
+                            endOfFile = false;
+                        }		
+                        // breaks out of the for-loop, there's no need to check further for more drops.
+                        j = windowSize + 1;
+                    }
+                } catch(ClassNotFoundException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            
+            
+            // If the endOfFile packet is sent, create and send the EOT packet
+            if(endOfFile)
+            {
+                // This will basically be its own mini sender and receiver that uses a while-loop to wait for ack.
+                //String endOfFileS = "XEOF";
+                packet eofpayLoad = new packet(3,0,0,null);
+                ByteArrayOutputStream eofStream = new ByteArrayOutputStream();
+                ObjectOutputStream ooeofStream = new ObjectOutputStream(eofStream);
+                ooeofStream.writeObject(eofpayLoad);
+                ooeofStream.flush();
+                byte[] eofSig = eofStream.toByteArray();
+                DatagramPacket sendEOF = new DatagramPacket(eofSig, eofSig.length, emulatorName, sendToEmulator);
+                toServer.send(sendEOF);
+                //ackLog.info(Integer.toString(sendEOF.getSeqNum()%8));
+                // Start timer and look for ack of EOT packet from server
+                
+            }
+        }//end while
+        toServer.close();	// Connections terminated.
+        fromServer.close();
+    }//end main
 }//end client
-
--- server.java
+++ server.java
@@ -1,102 +1,131 @@
 import java.io.*;
 import java.net.*;
 import java.util.Formatter;
+import java.io.Serializable.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class server
 {	// java server localhost 3000 3001 output.txt
-	public static void main(String args[]) throws IOException
-	{
-		InetAddress emulatorName = InetAddress.getByName(args[0]);
-		int receiveFromEmulator = Integer.parseInt(args[1]);
-		int sendToEmulator = Integer.parseInt(args[2]);
-		String fileName = args[3];
-        	ServerSocket nSocket = new ServerSocket(receiveFromEmulator);
-		//int packSize = 37;
-		boolean eot = false;
-		
-		// Creating UDP connection with Emulator.
-		DatagramSocket fromClient = new DatagramSocket(receiveFromEmulator);
-		DatagramSocket toClient = new DatagramSocket();
-		// Creating "received.txt" file.
-		Formatter file = new Formatter(fileName);
-		// using this byte array fixed the EOFException error I was getting.
-		byte[] incomingData = new byte[1024];
-		byte[] outgoingData = new byte[1024];
-		// expected sequence number of the first packet is always 0.
-		int exSeqNum = 0;
-		while(!eot)
-		{
-			// Receive packet from Client.
-			DatagramPacket recPack = new DatagramPacket(incomingData, incomingData.length);
-			fromClient.receive(recPack);
-			// Deserialize packet
-			byte[] data = recPack.getData();
-			ByteArrayInputStream baiStream = new ByteArrayInputStream(data);
-			ObjectInputStream oiStream = new ObjectInputStream(baiStream);
-			// Initialize the string to jump over any errors.
-			String received = new String();
-			try
-			{
-				packet packet = (packet) oiStream.readObject();
-				oiStream.close();
-				// if the packet is a data packet
-				if(packet.getType() == 1)
-				{
-					//System.out.println("Received packet, testing.");
-					// % 8 means the client doens't have to reset the seq #s every time.
-					if((exSeqNum % 8) == packet.getSeqNum())
-					{
-						// If the packet contained the expected seq #, send ack to server, inc exSeqNum
-						received = new String(packet.getData());
-						// send ack
-						//packet.printContents();
-						packet ack = new packet(0,(packet.getSeqNum()),0,null);
-						ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
-						ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
-						ooStream.writeObject(ack);
-						ooStream.flush();
-						outgoingData = baoStream.toByteArray();
-						DatagramPacket sendAck = new DatagramPacket(outgoingData, outgoingData.length, emulatorName, sendToEmulator);
-						toClient.send(sendAck);
-						// Write data received to the file.
-						file.format("%s", received);
-						exSeqNum++;
-					}
-					else // if the exSeqNum != packet.getSeqNum()
-					{
-						int ackNum = 0;
-						// fixes the issue where -1 was being sent as an ack #.
-						if(exSeqNum == 0){ackNum = 0;}
-						else{ackNum = (exSeqNum % 8) - 1;}
-						packet ack = new packet(0,ackNum,0,null);
-						ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
-						ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
-						ooStream.writeObject(ack);
-						ooStream.flush();
-						outgoingData = baoStream.toByteArray();
-						DatagramPacket sendAck = new DatagramPacket(outgoingData, outgoingData.length, emulatorName, sendToEmulator);
-						toClient.send(sendAck);
-						exSeqNum = 0;
-					}
-				}
-				// packet is an EOT from the client
-				else if(packet.getType() == 3)
-				{
-					System.out.println("Eot received, closing file and connections.");
-					eot = true;
-					file.close();		// Close output file.
-					fromClient.close();	// Close UDP socket.
-					toClient.close();
-				}
-				else
-				{
-					System.out.println("Error, unexpected packet type sent to server.");
-				}
-			} catch (ClassNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-		}//end while
-	}//end main
-}//end server
+    public static void main(String args[]) throws IOException
+    {
+        InetAddress emulatorName = InetAddress.getByName(args[0]);
+        int receiveFromEmulator = Integer.parseInt(args[1]);
+        int sendToEmulator = Integer.parseInt(args[2]);
+        String fileName = args[3];
+        ServerSocket nSocket = new ServerSocket(receiveFromEmulator);
+        //int packSize = 37;
+        boolean eot = false;
+        
+        // Arrival Log file
+        Logger arrivalLog = Logger.getLogger("arrival");
+        FileHandler fhArrival;
+        
+        try {
+            
+            // This block configure the logger with handler and formatter
+            fhArrival = new FileHandler("%h/Downloads/arrival.log"); // save the log file
+            arrivalLog.addHandler(fhArrival);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fhArrival.setFormatter(formatter);
 
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+        // Creating UDP connection with Emulator.
+        DatagramSocket fromClient = new DatagramSocket(receiveFromEmulator);
+        DatagramSocket toClient = new DatagramSocket();
+        // Creating "received.txt" file.
+        Formatter file = new Formatter(fileName);
+        // using this byte array fixed the EOFException error I was getting.
+        byte[] incomingData = new byte[1024];
+        byte[] outgoingData = new byte[1024];
+        // expected sequence number of the first packet is always 0.
+        int exSeqNum = 0;
+        while(!eot)
+        {
+            // Receive packet from Client.
+            DatagramPacket recPack = new DatagramPacket(incomingData, incomingData.length);
+            fromClient.receive(recPack);
+            // Deserialize packet
+            byte[] data = recPack.getData();
+            ByteArrayInputStream baiStream = new ByteArrayInputStream(data);
+            ObjectInputStream oiStream = new ObjectInputStream(baiStream);
+            // Initialize the string to jump over any errors.
+            String received = new String();
+            try
+            {
+                packet packet = (packet) oiStream.readObject();
+                oiStream.close();
+                // if the packet is a data packet
+                if(packet.getType() == 1)
+                {
+                    //System.out.println("Received packet, testing.");
+                    // % 8 means the client doens't have to reset the seq #s every time.
+                    if((exSeqNum % 8) == packet.getSeqNum())
+                    {
+                        //if (packet.getLength() != 0)
+                        //{
+                            // Update Arrival log file
+                            arrivalLog.info(Integer.toString(packet.getSeqNum()%8));
+                        //}
+                        
+                        // If the packet contained the expected seq #, send ack to server, inc exSeqNum
+                        received = new String(packet.getData());
+                        
+                        // send ack
+                        //packet.printContents();
+                        packet ack = new packet(0,(packet.getSeqNum()),0,null);
+                        ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+                        ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
+                        ooStream.writeObject(ack);
+                        ooStream.flush();
+                        outgoingData = baoStream.toByteArray();
+                        DatagramPacket sendAck = new DatagramPacket(outgoingData, outgoingData.length, emulatorName, sendToEmulator);
+                        toClient.send(sendAck);
+                        // Write data received to the file.
+                        file.format("%s", received);
+                        exSeqNum++;
+                    }
+                    else // if the exSeqNum != packet.getSeqNum()
+                    {
+                        int ackNum = 0;
+                        // fixes the issue where -1 was being sent as an ack #.
+                        if(exSeqNum == 0){ackNum = 0;}
+                        else{ackNum = (exSeqNum % 8) - 1;}
+                        packet ack = new packet(0,ackNum,0,null);
+                        ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+                        ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
+                        ooStream.writeObject(ack);
+                        ooStream.flush();
+                        outgoingData = baoStream.toByteArray();
+                        DatagramPacket sendAck = new DatagramPacket(outgoingData, outgoingData.length, emulatorName, sendToEmulator);
+                        toClient.send(sendAck);
+                        exSeqNum = 0;
+                    }
+                }
+                // packet is an EOT from the client
+                else if(packet.getType() == 3)
+                {
+                    System.out.println("Eot received, closing file and connections.\n");
+                    eot = true;
+                    file.close();		// Close output file.
+                    fromClient.close();	// Close UDP socket.
+                    toClient.close();
+                }
+                else
+                {
+                    System.out.println("Error, unexpected packet type sent to server.\n");
+                }
+            } catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }//end while
+    }//end main
+}//end server
